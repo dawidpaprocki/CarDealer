8164facfa2c4c93e8fa56e680f30b8c3a3d34ed8
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-03-08 00:33:07 +0100

    added worker  validation

--- src/main/java/com/example/car_dealer/controller/WorkerController.java
+++ src/main/java/com/example/car_dealer/controller/WorkerController.java
@@ -7,11 +7,13 @@ import com.example.car_dealer.service.UserService;
 import com.example.car_dealer.service.WorkerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,24 +33,28 @@ public class WorkerController {
     public String initAddWorker(
             Model model
     ) {
-        model.addAttribute("worker", new Worker());
+        model.addAttribute("worker", new WorkerDto());
         model.addAttribute("User", new User());
         return "addWorker";
     }
 
     @PostMapping("/add")
-    public String addWorker(@ModelAttribute("worker") WorkerDto workerDto,
-                            @ModelAttribute("user") User user,
+    public String addWorker(@Valid @ModelAttribute("worker") WorkerDto workerDto,     BindingResult bindingResultWorker,
+                            @Valid @ModelAttribute("user") User user,     BindingResult bindingResultUser,
+
                             Model model) throws ParseException {
+
+        if (bindingResultWorker.hasErrors() || bindingResultUser.hasErrors()) {
+            return "addWorker";
+        }
         Worker addedWorker = new Worker();
         addedWorker.setName(workerDto.getName());
         addedWorker.setSurName(workerDto.getSurName());
         addedWorker.setAddress(workerDto.getAddress());
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date parse = simpleDateFormat.parse(workerDto.getHiredDate());
+        addedWorker.setHiredDate(simpleDateFormat.parse(workerDto.getHiredDate()));
 
-        addedWorker.setHiredDate(parse);
         workerService.addWorkerToDB(addedWorker);
 
         User addedUser = new User();
--- src/main/java/com/example/car_dealer/dtos/WorkerDto.java
+++ src/main/java/com/example/car_dealer/dtos/WorkerDto.java
@@ -3,12 +3,23 @@ package com.example.car_dealer.dtos;
 import com.example.car_dealer.model.BaseModel;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
-public class WorkerDto extends BaseModel {
+public class WorkerDto {
+    @NotNull
+    @Size(min=1,max =150)
     private String name;
+    @NotNull
+    @Size(min=1,max =150)
     private String surName;
+    @NotNull
+    @Size(min=1,max =150)
     private String address;
+    @NotNull
     private String hiredDate;
 }
